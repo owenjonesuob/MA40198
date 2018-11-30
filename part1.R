9c237886cbd34178eb88346fcbb4f3f54dd64ab8
owenjonesuob <olj23@bath.ac.uk>
2018-11-30 14:19:17 +0000

    Add Q3

--- part1.R
+++ part1.R
@@ -5,7 +5,7 @@ rats <- read.table("http://people.bath.ac.uk/kai21/ASI/rats_data.txt")
 
 
 # Negative log-likelihood using values from built-in weibull functions
-nll_numeric <- function(theta, t, treated, status) {
+weib_nll_numeric <- function(theta, t, treated, status) {
 
   beta0 <- theta[1]
   beta1 <- theta[2]
@@ -24,7 +24,7 @@ nll_numeric <- function(theta, t, treated, status) {
 
 
 # Expressions for Weibull probability density function and Weibull survival function
-d_prob <- deriv(
+weib_prob <- deriv(
   expression(
     log((1/exp(log_sigma))/exp(beta0 + beta1*treated) * (t/exp(beta0 + beta1*treated))^((1/exp(log_sigma))-1) * exp(-(t/exp(beta0 + beta1*treated))^(1/exp(log_sigma))))
   ),
@@ -34,7 +34,7 @@ d_prob <- deriv(
 )
 
 
-d_surv <- deriv(
+weib_surv <- deriv(
   expression(
     -(t/exp(beta0 + beta1*treated))^(1/exp(log_sigma))
   ),
@@ -46,29 +46,29 @@ d_surv <- deriv(
 
 
 # Negative log-likelihood
-nll <- function(theta, t, treated, status) {
+weib_nll <- function(theta, t, treated, status) {
 
   beta0 <- theta[1]
   beta1 <- theta[2]
   log_sigma <- theta[3]
 
   -sum(
-    status * d_prob(t, beta0, beta1, log_sigma, treated),
-    (1-status) * d_surv(t, beta0, beta1, log_sigma, treated)
+    status * weib_prob(t, beta0, beta1, log_sigma, treated),
+    (1-status) * weib_surv(t, beta0, beta1, log_sigma, treated)
   )
 }
 
 
 # Gradient of negative log-likelihood
-nll_gr <- function(theta, t, treated, status) {
+weib_nll_gr <- function(theta, t, treated, status) {
 
   beta0 <- theta[1]
   beta1 <- theta[2]
   log_sigma <- theta[3]
 
   -colSums(rbind(
-    status * attr(d_prob(t, beta0, beta1, log_sigma, treated), "gradient"),
-    (1-status) * attr(d_surv(t, beta0, beta1, log_sigma, treated), "gradient")
+    status * attr(weib_prob(t, beta0, beta1, log_sigma, treated), "gradient"),
+    (1-status) * attr(weib_surv(t, beta0, beta1, log_sigma, treated), "gradient")
   ))
 }
 
@@ -79,10 +79,10 @@ theta0 <- c("beta0" = 2, "beta1" = 2, "log_sigma" = 2)
 
 
 # Minimise negative log-likelihood
-opt <- optim(
+weib_opt <- optim(
   par = theta0,
-  fn = nll,
-  gr = nll_gr,
+  fn = weib_nll,
+  gr = weib_nll_gr,
   t = rats$time,
   treated = rats$rx,
   status = rats$status,
@@ -93,10 +93,100 @@ opt <- optim(
 
 
 # Calculate standard errors from inverse Hessian
-std_err <- sqrt(diag(solve(opt$hessian)))
-names(std_err) <- c("beta0", "beta1", "log_sigma")
+weib_std_err <- sqrt(diag(solve(weib_opt$hessian)))
+names(weib_std_err) <- c("beta0", "beta1", "log_sigma")
 
 
 # 95% confidence interval for beta1
-# Note asymptotic distribution is
-opt$par[2] + c(-1, 1)*qnorm(0.975)*std_err[2]
+# Note asymptotic distribution is normal (for MLE estimate)
+weib_opt$par[2] + c(-1, 1)*qnorm(0.975)*weib_std_err[2]
+
+
+
+
+
+
+
+
+
+# Expressions for log-logistic probability density function and log-logistic survival function
+llog_prob <- deriv(
+  expression(
+    log((1/exp(log_sigma))/exp(beta0 + beta1*treated) * (t/exp(beta0 + beta1*treated))^((1/exp(log_sigma))-1) / (1 + (t/exp(beta0 + beta1*treated))^(1/exp(log_sigma)))^2)
+  ),
+  namevec = c("beta0", "beta1", "log_sigma"),
+  function.arg = c("t", "beta0", "beta1", "log_sigma", "treated"),
+  hessian = TRUE
+)
+
+
+llog_surv <- deriv(
+  expression(
+    -log(1 + (t/exp(beta0 + beta1*treated))^(1/exp(log_sigma)))
+  ),
+  namevec = c("beta0", "beta1", "log_sigma"),
+  function.arg = c("t", "beta0", "beta1", "log_sigma", "treated"),
+  hessian = TRUE
+)
+
+
+
+# Negative log-likelihood
+llog_nll <- function(theta, t, treated, status) {
+
+  beta0 <- theta[1]
+  beta1 <- theta[2]
+  log_sigma <- theta[3]
+
+  -sum(
+    status * llog_prob(t, beta0, beta1, log_sigma, treated),
+    (1-status) * llog_surv(t, beta0, beta1, log_sigma, treated)
+  )
+}
+
+
+# Gradient of negative log-likelihood
+llog_nll_gr <- function(theta, t, treated, status) {
+
+  beta0 <- theta[1]
+  beta1 <- theta[2]
+  log_sigma <- theta[3]
+
+  -colSums(rbind(
+    status * attr(llog_prob(t, beta0, beta1, log_sigma, treated), "gradient"),
+    (1-status) * attr(llog_surv(t, beta0, beta1, log_sigma, treated), "gradient")
+  ))
+}
+
+
+
+
+# Minimise negative log-likelihood
+llog_opt <- optim(
+  par = theta0,
+  fn = llog_nll,
+  gr = llog_nll_gr,
+  t = rats$time,
+  treated = rats$rx,
+  status = rats$status,
+  method = "BFGS",
+  hessian = TRUE,
+  control = list(trace = 1, maxit = 1000)
+)
+
+
+# Calculate standard errors from inverse Hessian
+llog_std_err <- sqrt(diag(solve(llog_opt$hessian)))
+
+# 95% confidence interval for each parameter
+# Note asymptotic distribution is normal
+data.frame(par = names(llog_opt$par),
+           val = llog_opt$par,
+           se = llog_std_err,
+           lower = llog_opt$par - qnorm(0.975)*llog_std_err,
+           upper = llog_opt$par + qnorm(0.975)*llog_std_err
+)
+
+
+
+
