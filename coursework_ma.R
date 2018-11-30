ef5fca74283cadfd74d0fe6176efd19b8006cb73
MaxAntson <34518205+MaxAntson@users.noreply.github.com>
2018-11-30 13:55:45 +0000

    Add files via upload

---
+++ coursework_ma.R
@@ -0,0 +1,62 @@
+rats <- read.table("http://people.bath.ac.uk/kai21/ASI/rats_data.txt")
+
+nll <- function(theta){
+  
+  l1 = dweibull(rats$time[rats$status==1&rats$rx==1], shape=1/exp(theta[3]), scale=exp(theta[1] + theta[2]), log=TRUE) 
+  l2 = dweibull(rats$time[rats$status==1&rats$rx==0], shape=1/exp(theta[3]), scale=exp(theta[1]), log=TRUE) 
+  
+  l3 = pweibull(rats$time[rats$status==0&rats$rx==1], shape=1/exp(theta[3]), scale=exp(theta[1] + theta[2]), log.p=TRUE, lower.tail=FALSE) 
+  l4 = pweibull(rats$time[rats$status==0&rats$rx==0], shape=1/exp(theta[3]), scale=exp(theta[1]), log.p=TRUE, lower.tail=FALSE)   
+  
+  -(sum(l1)+sum(l2)+sum(l3)+sum(l4))
+
+}
+
+
+theta0=c(1,1,1)
+
+q=optim(par=theta0,fn=nll, method="BFGS", hessian=TRUE)
+inv_hess = solve(q$hessian)
+
+#2. 
+q$par[2] + 1.96*sqrt(inv_hess[2,2])
+q$par[2] - 1.96*sqrt(inv_hess[2,2])
+
+#Interpretation: This shows that at the 5% significance level, beta1 is significantly different from 0 and therefore 
+#we can say that there is sufficient evidence to suggest that recieving treatment has an effect on the time it takes
+#for a tumour to appear. We have a negative estimate, which decreases the scale parameter in the Weibull distribution. 
+#This in turn DECREASES/INCREASES the esimated times for tumour appearance. 
+
+#3. 
+
+install.packages("actuar")
+library(actuar)
+
+nll_ll <- function(theta){
+  
+  shape = 1/exp(theta[3])
+  scale_0 = exp(theta[1])
+  scale_1 = exp(theta[1]+theta[2])
+  
+  l1 = log(shape)-log(scale_1)+(shape-1)*log(rats$time[rats$status==1&rats$rx==1]/scale_1) - 2*log(1+(rats$time[rats$status==1&rats$rx==1]/scale_1)^shape)
+  l2 = log(shape)-log(scale_0)+(shape-1)*log(rats$time[rats$status==1&rats$rx==0]/scale_0) - 2*log(1+(rats$time[rats$status==1&rats$rx==0]/scale_0)^shape)
+  
+  l3 = -log(1+(rats$time[rats$status==0&rats$rx==1]/scale_1)^shape)
+  l4 = -log(1+(rats$time[rats$status==0&rats$rx==0]/scale_0)^shape)
+  
+  -(sum(l1)+sum(l2)+sum(l3)+sum(l4))
+  
+}
+
+
+theta0_ll=c(1,1,1)
+q_ll=optim(par=theta0_ll,fn=nll_ll, method="BFGS", hessian=TRUE)
+inv_hess_ll = solve(q_ll$hessian)
+q_ll$par[2] + 1.96*sqrt(inv_hess_ll[2,2])
+q_ll$par[2] - 1.96*sqrt(inv_hess_ll[2,2])
+
+
+#4.
+
+
+=
