b1823d88f87376dc20ad6f9edd801ce6e94d96a3
owenjonesuob <olj23@bath.ac.uk>
2018-12-03 21:34:21 +0000

    Obtain sensible gamma

--- part2_oj.R
+++ part2_oj.R
@@ -3,59 +3,68 @@ fatigue <- read.table("https://people.bath.ac.uk/kai21/ASI/fatigue.txt")
 
 
 # Expressions for Weibull probability density function and Weibull survival function
+# l_gamma is logit (inverse sigmoid) transform of gamma, i.e.
+#  l_gamma = log(gamma / (min(fatigue$s) - gamma))
 N_prob <- deriv(
   expression(
-    log((1/exp(log_sigma))/(exp(log_alpha)*(s - gamma)^delta) * (y/(exp(log_alpha)*(s - gamma)^delta))^((1/exp(log_sigma))-1) * exp(-(y/(exp(log_alpha)*(s - gamma)^delta))^(1/exp(log_sigma))))
+    log(
+      (1/exp(log_sigma)) / (exp(log_alpha) * (s - min_s/(1 + exp(-l_gamma)))^delta) *
+        (y / (exp(log_alpha) * (s - min_s/(1 + exp(-l_gamma)))^delta))^((1/exp(log_sigma)) - 1) *
+        exp(-(y / (exp(log_alpha) * (s - min_s/(1 + exp(-l_gamma)))^delta))^(1/exp(log_sigma)))
+    )
   ),
-  namevec = c("log_alpha", "delta", "log_sigma"),
-  function.arg = c("y", "s", "gamma", "log_alpha", "delta", "log_sigma"),
+  namevec = c("log_alpha", "delta", "log_sigma", "l_gamma"),
+  function.arg = c("y", "s", "min_s", "log_alpha", "delta", "log_sigma", "l_gamma"),
   hessian = TRUE
 )
 
 
 N_surv <- deriv(
   expression(
-    -(y/(exp(log_alpha)*(s - gamma)^delta))^(1/exp(log_sigma))
+    -(y / (exp(log_alpha) * (s - min_s/(1 + exp(-l_gamma)))^delta))^(1/exp(log_sigma))
   ),
-  namevec = c("log_alpha", "delta", "log_sigma"),
-  function.arg = c("y", "s", "gamma", "log_alpha", "delta", "log_sigma"),
+  namevec = c("log_alpha", "delta", "log_sigma", "l_gamma"),
+  function.arg = c("y", "s", "min_s", "log_alpha", "delta", "log_sigma", "l_gamma"),
   hessian = TRUE
 )
 
 
 
 # Negative log-likelihood
-N_nll <- function(theta, y, stress, runout, gamma) {
+N_nll <- function(theta, y, stress, runout) {
 
   log_alpha <- theta[1]
   delta <- theta[2]
   log_sigma <- theta[3]
+  l_gamma <- theta[4]
 
   -sum(
-    (1-runout) * N_prob(y, stress, gamma, log_alpha, delta, log_sigma),
-    runout * N_surv(y, stress, gamma, log_alpha, delta, log_sigma)
+    (1-runout) * N_prob(y, stress, min(stress), log_alpha, delta, log_sigma, l_gamma),
+    runout * N_surv(y, stress, min(stress), log_alpha, delta, log_sigma, l_gamma)
   )
 }
 
 
 # Gradient of negative log-likelihood
-N_nll_gr <- function(theta, y, stress, runout, gamma) {
+N_nll_gr <- function(theta, y, stress, runout) {
 
   log_alpha <- theta[1]
   delta <- theta[2]
   log_sigma <- theta[3]
+  l_gamma <- theta[4]
 
   -colSums(rbind(
-    (1-runout) * attr(N_prob(y, stress, gamma, log_alpha, delta, log_sigma), "gradient"),
-    runout * attr(N_surv(y, stress, gamma, log_alpha, delta, log_sigma), "gradient")
+    (1-runout) * attr(N_prob(y, stress, min(stress), log_alpha, delta, log_sigma, l_gamma), "gradient"),
+    runout * attr(N_surv(y, stress, min(stress), log_alpha, delta, log_sigma, l_gamma), "gradient")
   ))
 }
 
 
 
 # Initial params
-theta0 <- c("log_alpha" = 2, "delta" = -2, "log_sigma" = 2)
-gamma <- 79.3#runif(1, min = 0, max = min(fatigue$s))
+# Certain combinations seem to lead to strange (non-optimal) results...
+# e.g. c(1, 1, 1, 1)
+theta0 <- c("log_alpha" = 2, "delta" = 1, "log_sigma" = 2, "l_gamma" = 0)
 
 # Minimise negative log-likelihood
 N_opt <- optim(
@@ -65,7 +74,6 @@ N_opt <- optim(
   y = fatigue$N,
   stress = fatigue$s,
   runout = fatigue$ro,
-  gamma = gamma,
   method = "BFGS",
   hessian = TRUE,
   control = list(trace = 1, maxit = 1000)
@@ -77,10 +85,13 @@ N_std_err <- sqrt(diag(solve(N_opt$hessian)))
 
 # 95% confidence interval for each parameter
 # Note asymptotic distribution is normal
-data.frame(
-  par = names(N_opt$par),
+round(data.frame(
   val = N_opt$par,
   se = N_std_err,
   lower = N_opt$par - qnorm(0.975)*N_std_err,
   upper = N_opt$par + qnorm(0.975)*N_std_err
-)
+), 3)
+
+
+# Recover gamma
+min(fatigue$s) / (1 + exp(-N_opt$par[4]))
