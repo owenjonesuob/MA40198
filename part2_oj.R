d2b807ba7243918a6f2596d465f1c8e6429518cf
owenjonesuob <olj23@bath.ac.uk>
2018-12-10 14:28:36 +0000

    Add mcmc_mh_cov_f

--- part2_oj.R
+++ part2_oj.R
@@ -115,3 +115,214 @@ ggplot(fatigue, aes(x = s)) +
   stat_function(fun = function(s) {alpha * (s - gamma)^delta * qweibull(0.5, shape = 1, scale = 1)}, aes(color = "50% quantile"), linetype = "dashed") +
   ggtitle("Estimated quantiles of fatigue") + labs(x = "Stress", y = "N", color = "Estimated quantiles") +
   theme_bw() + theme(legend.position = c(0.8, 0.85), legend.background = element_rect(color = "black"))
+
+
+
+
+log_posterior <- function(theta, y, b, stress, runout) {
+
+  log_alpha <- theta[1]
+  delta <- theta[2]
+  log_sigma <- theta[3]
+  mu_gamma <- theta[4]
+  log_sigma_gamma <- theta[5]
+
+  l_gamma <- log(b / (min(stress)-b))
+
+  # Log conditional density of y given b
+  lfy_b <- sum(
+    (1-runout) * N_prob(y, stress, min(stress), log_alpha, delta, log_sigma, l_gamma),
+    runout * N_surv(y, stress, min(stress), log_alpha, delta, log_sigma, l_gamma)
+  )
+
+  # Log marginal density of b
+  lfb <- sum(dweibull(x = b, shape = 1/exp(log_sigma_gamma), scale = exp(mu_gamma), log = TRUE))
+
+  # Log joint density of y and b is the sum (joint density is product - y, b are independent)
+  lf <- lfy_b + lfb
+
+  # Define log-prior (sum of log-priors for all parameters - here, just for log_sigma_b)
+  log_prior <- dexp(exp(log_sigma_gamma), rate = 5, log = TRUE)
+
+  # Log-posterior is log-prior plus log-likelihood
+  lf + log_prior
+
+}
+
+
+log_posterior(c(1, 1, 1, 1, 1), fatigue$N, rep(0.1, 26), fatigue$s, fatigue$runout)
+
+
+
+
+
+
+
+mcmc_mh_f <- function(iters, burnin, init_params, init_bs, tuners, b_tuner, y, stress, runout, show_plot = TRUE) {
+
+  theta_vals <- matrix(NA, nrow = iters+1, ncol = 5)
+  theta_vals[1, ] <- init_params
+
+  b_vals <- matrix(NA, nrow = iters+1, ncol = 26)
+  b_vals[1, ] <- init_bs
+
+  acceptance <- rep(NA, iters)
+  acceptance_b <- rep(NA, iters)
+
+  log_post <- log_posterior(init_params, y, b_vals[1, ], stress, runout)
+
+  # Progress bar in console
+  pb <- txtProgressBar(min = 0, max = iters, style = 3)
+
+
+  # MCMC loop
+  for (k in seq_len(iters)) {
+
+
+    # "Non-random" parameters
+    theta_prop <- rnorm(5, mean = theta_vals[k, ], sd = tuners)
+
+
+    log_post_prop <- log_posterior(theta_prop, y, b_vals[k, ], stress, runout)
+
+    accept_prob <- exp(log_post_prop - log_post)
+
+    if (accept_prob > runif(1)) {
+
+      theta_vals[k+1, ] <- theta_prop
+      log_post <- log_post_prop
+      acceptance[k] <- TRUE
+
+    } else {
+
+      theta_vals[k+1, ] <- theta_vals[k, ]
+      acceptance[k] <- FALSE
+    }
+
+
+    # "Random" parameters (proposed/accepted separately)
+
+    # Now hold other parameters steady
+    # Random effects have standard deviation exp(log_sigma_b)
+    b_prop <- rnorm(26, mean = b_vals[k, ], sd = b_tuner)
+
+    # Use (possibly newly accepted) values of theta
+    log_post_prop_b <- log_posterior(theta_vals[k+1, ], y, b_prop, stress, runout)
+
+    accept_prob_b <- exp(log_post_prop_b - log_post)
+
+    if (accept_prob_b > runif(1)) {
+
+      b_vals[k+1, ] <- b_prop
+      log_post <- log_post_prop_b
+      acceptance_b[k] <- TRUE
+
+    } else {
+
+      b_vals[k+1, ] <- b_vals[k, ]
+      acceptance_b[k] <- FALSE
+    }
+
+    setTxtProgressBar(pb, value = k)
+  }
+
+
+  if (show_plot) {
+    par(mfrow = c(2, 3))
+    apply(theta_vals, 2, function(x) {
+      plot(x, type = "l")
+      abline(v = burnin, col = "red")
+      rect(0, min(x), burnin, min(y), density = 10, col = "red")
+    })
+    par(mfrow = c(1, 1))
+  }
+
+  close(pb)
+  cat(sprintf("Total acceptance:       %2.3f%%\n", mean(acceptance)*100))
+  cat(sprintf("Burned-in acceptance:   %2.3f%%\n", mean(acceptance[-(1:burnin)])*100))
+  cat(sprintf("Burned-in b acceptance: %2.3f%%\n", mean(acceptance_b[-(1:burnin)])*100))
+
+  list(theta = theta_vals, b = b_vals)
+}
+
+
+pilot <- mcmc_mh_f(50000, 15000, c(18, -2, -1, 4, -2), rep(66, 26), rep(0.025, 5), 0.2, fatigue$N, fatigue$s, fatigue$ro, FALSE)
+
+
+D <- cbind(pilot$theta, pilot$b)[15001:50001, ]
+
+cov_D <- cov(D)
+
+
+
+
+
+
+mcmc_mh_cov_f <- function(iters, burnin, init_params, init_bs, cov_matrix, tuner, y, stress, runout, show_plot = TRUE) {
+
+  theta_vals <- matrix(NA, nrow = iters+1, ncol = length(init_params))
+  theta_vals[1, ] <- init_params
+
+  b_vals <- matrix(NA, nrow = iters+1, ncol = length(init_bs))
+  b_vals[1, ] <- init_bs
+
+  acceptance <- rep(NA, iters)
+
+  log_post <- log_posterior(init_params, y, b_vals[1, ], stress, runout)
+
+  # Progress bar in console
+  pb <- txtProgressBar(min = 0, max = iters, style = 3)
+
+
+  # MCMC loop
+  for (k in seq_len(iters)) {
+
+
+    prop <- MASS::mvrnorm(1, mu = c(theta_vals[k, ], b_vals[k, ]), Sigma = (tuner^2 * cov_matrix))
+    theta_prop <- prop[1:length(init_params)]
+    b_prop <- prop[(length(init_params)+1):length(prop)]
+
+
+    log_post_prop <- log_posterior(theta_prop, y, b_prop, stress, runout)
+
+    accept_prob <- exp(log_post_prop - log_post)
+
+    if (accept_prob > runif(1)) {
+
+      theta_vals[k+1, ] <- theta_prop
+      b_vals[k+1, ] <- b_prop
+      log_post <- log_post_prop
+      acceptance[k] <- TRUE
+
+    } else {
+
+      theta_vals[k+1, ] <- theta_vals[k, ]
+      b_vals[k+1, ] <- b_vals[k, ]
+      acceptance[k] <- FALSE
+    }
+
+
+    setTxtProgressBar(pb, value = k)
+  }
+
+
+  if (show_plot) {
+    par(mfrow = c(2, 3))
+    apply(theta_vals, 2, function(x) {
+      plot(x, type = "l")
+      abline(v = burnin, col = "red")
+      rect(0, min(x), burnin, min(y), density = 10, col = "red")
+    })
+    par(mfrow = c(1, 1))
+  }
+
+  close(pb)
+  cat(sprintf("Total acceptance:       %2.3f%%\n", mean(acceptance)*100))
+  cat(sprintf("Burned-in acceptance:   %2.3f%%\n", mean(acceptance[-(1:burnin)])*100))
+
+  list(theta = theta_vals, b = b_vals)
+}
+
+
+
+zz <- mcmc_mh_cov_f(5000, 1000, tail(pilot$theta, 1), tail(pilot$b, 1), cov_D, 0.35, fatigue$N, fatigue$s, fatigue$ro)
